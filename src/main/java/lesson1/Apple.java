dd61153cbaa67f96c9eaf1a91d2cb559d720d5ae
RuslanV <vdovin_ruslan@mail.ru>
2020-12-16 17:55:32 +0300

    Домашнее задание по 1 уроку

---
+++ src/main/java/lesson1/Apple.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class Apple extends Fruit {
+
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "apples";
+    }
+}
---
+++ src/main/java/lesson1/Orange.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class Orange extends Fruit {
+
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "oranges";
+    }
+
+}
